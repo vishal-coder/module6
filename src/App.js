cfae89f72d2b7d01ff7552efbf02e39dadf95762
vishal Coder <[vishal2app@gmail.com]>
2022-06-01 16:18:51 +0530

    class task 1 june movie list static

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const name = "Guvi";
-
-  // array of objects
-  const users = [
-    { name: "jack", imgSrc: "./1.jpg" },
-    { name: "jack", imgSrc: "./2.jpg" },
-    { name: "jack", imgSrc: "./3.jpg" },
+  const MovieList = [
+    {
+      image:
+        "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+      title: "RRR",
+      rating: "⭐ 8.8",
+      info: "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments",
+    },
+    {
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+      title: "Iron Man 2",
+      rating: "⭐ 7",
+      info: "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+    },
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+      title: "No Country for Old Men",
+      rating: "⭐ 7",
+      info: "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+    },
+    {
+      image:
+        "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+      title: "Jai Bhim",
+      rating: "⭐ 8.8",
+      info: "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+    },
   ];
   return (
     <div className="App">
-      {users.map((user) => (
-        <Message name={user.name} imgSrc={user.imgSrc} />
-      ))}
+      <div className="movieList">
+        {MovieList.map((movie) => (
+          <MovieCard
+            imgSrc={movie.image}
+            title={movie.title}
+            rating={movie.rating}
+            info={movie.info}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
-function Message(props) {
+function MovieCard(props) {
   return (
-    <div>
-      <img
-        className="profile-pic"
-        src={props.imgSrc}
-        alt=""
-        width="200"
-        height="200"
-      />
-      <h1>Hello, {props.name}</h1>
+    <div className="MovieCard">
+      <img className="movie-Photo" src={props.imgSrc} alt="" />
+      <div className="movie-specs">
+        <h2>{props.title}</h2>
+        <div>{props.rating}</div>
+      </div>
+
+      <p className="summary">{props.info}</p>
     </div>
   );
 }
-
 export default App;
